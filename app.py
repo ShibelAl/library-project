3bc9f8e196f35583ed29ff29d516278ba47c31c2
ShibelAl <shibelalshech300@gmail.com>
2024-06-19 20:31:34 +0300

    added books function, that handles the display and submission of books in the library.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from library import Library
+from book import Book
 
 # This Flask application serves as the foundation for the library website.
 
@@ -11,10 +12,10 @@ def index():
     """
     Renders the homepage template
 
-    Returns:
+    :returns:
         The rendered HTML content for the homepage.
 
-    Raises:
+    :raises:
         TemplateNotFound: If the `index.html` template is not found.
     """
 
@@ -22,6 +23,38 @@ def index():
 
 
 
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    """
+    Handle the display and submission of books in the library.
+
+    For GET requests, this function retrieves and displays a list of books
+    from the library. If provided, it filters the books by the author, genre and the publication year.
+
+    For POST requests, this function handles the submission of a new book
+    to the library. The new book's details are extracted from the form data
+    and added to the library.
+
+    Form Data (POST):
+        title (str): The title of the book.
+        author (str): The author of the book.
+        year (int): The publication year of the book.
+        genre (str): The genre of the book.
+
+    :returns:
+        - For GET requests: Renders the 'books.html' template with the list of books.
+        - For POST requests: Redirects to the 'books' route after adding the new book.
+    """
+    if request.method == 'POST':
+        data = request.form
+        book = Book(data['title'], data['author'], int(data['year']), data['genre'])
+        library.add_book(book)
+        return redirect(url_for('books'))
+
+    # If I want to see only the books published in the 2008
+    # I should put -> publication_year=2008  in the list_books function.
+    listed_books = library.list_books()
+    return render_template('books.html', books=listed_books)
 
 
 if __name__ == '__main__':
